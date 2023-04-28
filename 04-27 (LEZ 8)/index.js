23555767406dede89db73c72ab26f6abfe3f4e5e
zAiro12 <lucaairoldi92@gmail.com>
2023-04-28 09:54:22 +0200

    aggiunto swagger

--- 04-27 (LEZ 8)/index.js
+++ 04-27 (LEZ 8)/index.js
@@ -1,47 +1,92 @@
+const { raw } = require('body-parser')
 const express = require('express')
+const fs = require('fs')
+
+let rawUsers = fs.readFileSync('users.json') 
+var users = JSON.parse(rawUsers)
+console.log(users)
+
 const app = express()
+app.use(express.json())
 const apiKey = "123456"
-var users = [
-    {id: 1, name: "Luca", surname: "Airoldi"}, 
-    {id: 2, name: "Sofia", surname: "Zanelli"}
-]
-
-app.get('/', function(req, res) {
-    res.send('root')
-})
 
 
-app.get('/users', function(req, res) {
-    if (req.query.apikey == apiKey){
+function checkApiKey(res, userApiKey) {
+    if (userApiKey == apiKey) {
+        return true
+    }
+    res.status(401).send("Non Autorizzato")
+}
+
+function updateFile(){
+    let usersRaw = JSON.stringify(users)
+    fs.writeFileSync("users.json", usersRaw)
+}
+
+function addUser(user) {
+    user.id = users.length + 1
+    if (user.name == undefined) {
+        res.status(400).send("Missing Name")
+        return
+    }
+    if (user.surname == undefined) {
+        res.status(400).send("Missing Surname")
+        return
+    }
+
+    users.push(user)
+    updateFile()
+}
+
+function updateUser(res, id, updatedUser) {
+    let index = users.findIndex(user => user.id == id)
+    if (index == -1) {
+        res.status(404).send("User not found")
+        return
+    }
+    if (updatedUser.id != id) {
+        res.status(400).send("Wrong Id")
+        return
+    }
+  
+    if (updatedUser.id == undefined) {
+        res.status(400).send("Missing Id")
+        return
+    }
+    if (updatedUser.name == undefined) {
+        res.status(400).send("Missing Name")
+        return
+    }
+    if (updatedUser.surname == undefined) {
+        res.status(400).send("Missing Surname")
+        return
+    }
+    users[index] = updatedUser
+    updateFile()
+    res.json(users)
+}
+
+app.get('/users', function (req, res) {
+    if (checkApiKey(res, req.query.apikey)) {
         res.json(users)
-    }else{
-        res.status(401).send("NON AUTORIZZATO")
     }
 })
 
-app.get('/users/:id', function(req, res) {
-   var user = users.filter(user => user.id == req.params.id)
-   res.json(user)
+app.get('/users/:id', function (req, res) {
+    // Ricerca nel database
+    if (checkApiKey(res, req.query.apikey)) {
+        var user = users.filter(user => user.id == req.params.id)
+        res.json(user)
+    }
 })
 
-app.post("/users/", function(req, res) {
+app.post("/users", function (req, res) {
     addUser(req.body)
     res.json(users)
 })
 
-app.put("/users/", function(req, res) {
-    updateUser(req.body)
-    res.json(users)
+app.put("/users/:id", function (req, res) {
+    updateUser(res, req.params.id, req.body)
 })
 
-app.listen(3000)
-
-console.log("ONLINE")
-
-function addUser(user){
-    users.push(user)
-}
-
-function updateUser(user){
-    index = users.findIndex(uers => user.id)
-}
+app.listen(3100)
